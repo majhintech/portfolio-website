1133933192ef84f0695f8ac54132346cbc0cdbc7
MAJHI <122159208+majhintech@users.noreply.github.com>
2023-08-26 09:22:29 +0000

    working on feature section

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,5 +1,5 @@
 // Grab Elements
-const selectElement = selector => {
+const selectElement = (selector) => {
   const element = document.querySelector(selector)
   if(element) return element;
   throw new Error (`Something went wrong, make sure that ${selector} exists or is typed correctly.`);
@@ -7,7 +7,7 @@ const selectElement = selector => {
 
 // Nav style on scroll 
 const scrollHeader = () => {
-  const headerElement = selectElement('.js-header');
+  const headerElement = selectElement('#header');
   if (this.scrollY >= 15) {
     headerElement.classList.add('activated');
   } else {
@@ -18,3 +18,31 @@ const scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader);
 
 // open menu & search popup
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+
+const toggleMenu = () => {
+  const mobileMenu = selectElement('#menu');
+  mobileMenu.classList.toggle('activated');
+  menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
+
+// switch between theme/add local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+if (currentTheme) {
+  bodyElement.classList.add('light-theme');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+  bodyElement.classList.toggle('light-theme');
+
+  if (bodyElement.classList.contains) {
+    localStorage.setItem('currentTheme', 'themeActive');
+  } else {
+    localStorage.removeItem('currentTheme');
+  }
+});
